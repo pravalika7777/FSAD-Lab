3b64c05623843a501b4a052a87811dc6f8a51245
Harsha pravalika <pharshapravalika@gmail.com>
2026-03-14 10:33:42 +0530

    Delete src/com/example/autowire directory

--- src/com/example/autowire/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.autowire;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.example.autowire")
-public class AppConfig {
-}
